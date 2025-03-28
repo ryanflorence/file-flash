27272b29dc1672175404b9aa5b6a01e60051c890
Ryan Florence <rpflorence@gmail.com>
2025-03-28 14:40:57 -0600

    remove weird terraform stuff

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "droplet_ip" {
-  description = "The public IP address of the droplet"
-  value       = digitalocean_droplet.file_flash.ipv4_address
-}
-
-output "droplet_name" {
-  description = "The name of the droplet"
-  value       = digitalocean_droplet.file_flash.name
-} 
